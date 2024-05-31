ad4d53ee280383490d49d8dac4d9c44cc9f9e2d9
Евгения <april-may@mail.ru>
2024-05-31 14:27:34 +0300

    Вносит изменения в меню mobile: hover, js

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,18 +1,18 @@
 /* в этот файл добавляет скрипты*/
 
-// const mainNav = document.querySelector('.main-nav');
-// const navToggle = document.querySelector('.main-nav__toggle');
+const mainNav = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
-// mainNav.classList.remove('main-nav--no-js');
+mainNav.classList.remove('main-nav--no-js');
 
-// mainNav.classList.add('main-nav--open');
+mainNav.classList.add('main-nav--open');
 
-// navToggle.addEventListener('click', () => {
-//   if (mainNav.classList.contains('main-nav--open')) {
-//     mainNav.classList.remove('main-nav--open');
-//     mainNav.classList.add('main-nav--close');
-//   } else {
-//     mainNav.classList.add('main-nav--open');
-//     mainNav.classList.remove('main-nav--close');
-//   }
-// });
+navToggle.addEventListener('click', () => {
+  if (mainNav.classList.contains('main-nav--open')) {
+    mainNav.classList.remove('main-nav--open');
+    mainNav.classList.add('main-nav--close');
+  } else {
+    mainNav.classList.add('main-nav--open');
+    mainNav.classList.remove('main-nav--close');
+  }
+});
