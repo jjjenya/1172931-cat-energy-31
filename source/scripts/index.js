ad92fa499df7fb4c9e6e4542e42c79aa0e6fb9ce
Сабирова Евгения <54816340+jjjenya@users.noreply.github.com>
2024-06-01 18:07:13 +0300

    Начинает адаптивные сетки и декоратинвые элементы (index) (#14)
    * Вносит правки: текст, слайдер, global, пути до файлов; дополняет image-set
    
    * Добавляет меню mobile
    
    * Вносит изменения в меню mobile
    
    * Вносит изменения в меню mobile: hover, js
    
    * Оформляет меню и блок intro
    
    * Выполняет блок content
    
    * Выполняет блок work
    
    * Начинает выполнять блок stats

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,18 @@
 /* в этот файл добавляет скрипты*/
+
+const mainNav = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
+
+mainNav.classList.remove('main-nav--no-js');
+
+mainNav.classList.add('main-nav--open');
+
+navToggle.addEventListener('click', () => {
+  if (mainNav.classList.contains('main-nav--open')) {
+    mainNav.classList.remove('main-nav--open');
+    mainNav.classList.add('main-nav--close');
+  } else {
+    mainNav.classList.add('main-nav--open');
+    mainNav.classList.remove('main-nav--close');
+  }
+});
